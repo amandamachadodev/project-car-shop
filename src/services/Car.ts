ee7cc0f98824192558946496b632085b73290714
mandioquynha <machado.amandag@gmail.com>
2022-09-14 18:26:45 -0300

    feat: :sparkles: rota get /cars
    requisito 8 concluido

--- src/services/Car.ts
+++ src/services/Car.ts
@@ -17,6 +17,10 @@ class CarService implements IService<ICar> {
     }
     return this._car.create(obj);
   }
+
+  public async read(): Promise<ICar[]> {
+    return this._car.read();
+  }
 }
 
 export default CarService;
