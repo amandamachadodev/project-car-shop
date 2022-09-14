6aa8ab7d222530639889026854c562fceafbe0e6
mandioquynha <machado.amandag@gmail.com>
2022-09-14 19:03:46 -0300

    feat: :sparkles: rota get /cars/id
    requisito 9 concluido

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -22,4 +22,12 @@ export default class CarController {
     const results = await this._service.read();
     return res.status(200).json(results);
   }
+
+  public async readOne(
+    req: Request,
+    res: Response<ICar | null>,
+  ) {
+    const result = await this._service.readOne(req.params.id);
+    return res.status(200).json(result);
+  }
 }
